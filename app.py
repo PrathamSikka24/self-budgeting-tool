7843d9cac69588a71c3276b575e5383ce97f5ef2
Pratham Sikka <prathamsikka2004@gmail.com>
2024-03-05 15:40:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,25 @@
 from flask import Flask, request, jsonify
-from models import db, Account, Transaction  
+from flask_sqlalchemy import SQLAlchemy
 import uuid
-from datetime import date
+from datetime import datetime
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///yourdatabase.db'  # I will add this
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///yourdatabase.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db.init_app(app)
+db = SQLAlchemy(app)
+
 
 @app.route('/accounts', methods=['POST'])
 def add_account():
+    # Implementation of adding an account
     pass
 
 @app.route('/accounts', methods=['GET'])
 def get_accounts():
+    # Implementation of fetching accounts
     pass
-    
+
 if __name__ == "__main__":
+    from models import *  
+    db.create_all()  
     app.run(debug=True)
