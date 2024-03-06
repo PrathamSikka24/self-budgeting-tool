e476babe0208a55b4bdc8a2eec122a1e4bec9f35
Pratham Sikka <prathamsikka2004@gmail.com>
2024-03-06 22:23:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -68,4 +68,4 @@ def add_transaction():
         return jsonify({"error": "Database connection failed"}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
